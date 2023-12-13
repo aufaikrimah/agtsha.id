07c97be206561a0d5a17d512db351b580cef5441
Aufa Ikrimah <aufaikrimah@gmail.com>
2023-12-13 17:56:00 +0700

    adding footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,13 @@
 function Footer() {
 
     return (
-        <h1>Footer</h1>
+        <footer className=" mt-14">
+            <div className="text-center">
+                <p className="text-sm">
+                    &copy; 2023 AGTSHA ID. All Rights Reserved.
+                </p>
+            </div>
+        </footer>
     );
 }
 
